2e54d5065ade6f6916f705699ad2ade9219ab6c7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-29 12:02:55 -0300

    Inclusão de texto

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -11,8 +11,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/Brunogabriel-dev/cart-list",
+    previewUrl: "https://carts-shop.netlify.app/",
   },
   {
     id: 2,
